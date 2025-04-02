304258893963568c5d44a7e04e024b2261b9682c
zerok-cell <swiftkey382@gmail.com>
2025-04-02 20:19:15 +0300

    Полученеи даты окончания прошлого месяца

---
+++ lib/utils/dayjs.instance.ts
@@ -0,0 +1,67 @@
+import dayjs from 'dayjs'
+import dayOfYear from 'dayjs/plugin/dayOfYear'
+import isLeapYear from 'dayjs/plugin/isLeapYear'
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
+import isToday from 'dayjs/plugin/isToday'
+import isTomorrow from 'dayjs/plugin/isTomorrow'
+import isYesterday from 'dayjs/plugin/isYesterday'
+import Calendar from 'dayjs/plugin/calendar'
+import 'dayjs/locale/ru';
+
+dayjs.locale('ru');
+/**
+ * @description Получить номер дня в году
+ */
+dayjs.extend(dayOfYear);
+
+/**
+ * @description Является ли год високосным
+ */
+dayjs.extend(isLeapYear);
+
+/**
+ * @description Является ли дата той же или более поздней.
+ */
+dayjs.extend(isSameOrAfter);
+
+/**
+ * @description Является ли дата той же или более ранней.
+ */
+dayjs.extend(isSameOrBefore);
+
+/**
+ * @description Является ли дата сегодняшним днем.
+ */
+dayjs.extend(isToday);
+/**
+ * @description Является ли дата завтрашним днем.
+ */
+dayjs.extend(isTomorrow);
+/**
+ * @description Является ли дата вчерашним днем.
+ */
+dayjs.extend(isYesterday);
+
+dayjs.extend(Calendar);
+
+
+const initialData = dayjs("2027-01-01").add(-1, "month");
+const maxDay = initialData.daysInMonth().toString();
+
+/**
+ * @description Дата на которую окончился прошлый месяц
+ */
+const newData = dayjs(`${initialData.year()}-${initialData.month() + 1}-${maxDay}`);
+
+/**
+ * Число ячеек которое надо зарезервировать или скрыть в новом месяце
+ */
+
+// console.log(newData.day())
+// console.log(newData.format("dddd"));
+
+
+export default dayjs;
+
+
